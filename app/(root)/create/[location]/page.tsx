1cf0ca8b5b341328268bf8e21478eb936749c6ad
Sdfeagt <rafalciechanski15@gmail.com>
2024-01-10 13:22:49 +0200

    Destination filter works

--- app/(root)/create/[location]/page.tsx
+++ app/(root)/create/[location]/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from '@/components/ui/button'
 import Destinations from '@/components/ui/destinations'
-import { Input } from '@/components/ui/input'
 import prismadb from '@/lib/prismadb'
-import { MapPin, Search } from 'lucide-react'
-import Image from 'next/image'
 
 interface LocationPageProps {
     params: { location: string }
@@ -19,18 +15,7 @@ const Location: React.FC<LocationPageProps> = async ({ params }) => {
 
     return (
         <div>
-            <div className='text-lg flex justify-center my-6'>Where are you travelling to?</div>
-            <div className="flex items-center rounded-full bg-figmaDark my-4 py-2">
-                <div className="bg-figmaLightGreen rounded-full p-2 m-2 flex items-center">
-                    <MapPin color="white" />
-                </div>
-                <Input type="search" placeholder="Search..." className='bg-figmaDark text-lg' />
-                <Button type="submit" className="bg-figmaLightDark rounded-full p-2 m-2 flex items-center">
-                    <Search color="white" />
-                </Button>
-            </div>
-            <div className='text-sm text-gray-400 mt-14 mb-4'>Popular destinations in {params.location}</div>
-            <Destinations destinations={destinationsFilter} />
+            <Destinations destinations={destinationsFilter} location={params.location} />
         </div>
     )
 }
--- components/ui/destinations.tsx
+++ components/ui/destinations.tsx
@@ -1,5 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { MapPin, Search } from 'lucide-react'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+} from "@/components/ui/form"
 
 type Destination = {
     name: string,
@@ -8,18 +23,77 @@ type Destination = {
 }
 interface DestinationsProps {
     destinations: Destination[],
+    location: string
+
+
 }
-const Destinations: React.FC<DestinationsProps> = ({ destinations }) => {
+
+const FormSchema = z.object({
+    dest: z.string(),
+})
+
+const Destinations: React.FC<DestinationsProps> = ({ destinations, location }) => {
+    const [filtered, setFiltered] = useState(destinations)
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            dest: "",
+        },
+    })
+
+    const onSubmit = (data: z.infer<typeof FormSchema>) => {
+        console.log("***");
+        console.log("data: " + data.dest);
+        console.log("Before set: " + filtered.length);
+        setFiltered(destinations)
+        console.log("After set: " + filtered.length);
+        if (data.dest != "") {
+            setFiltered(destinations.filter((dest) => dest.name.toLowerCase().includes(data.dest.toLowerCase())))
+        }
+    }
+
     return (
-        <div className='inline-grid grid-cols-2 gap-4'>
-            {destinations.map((dest) =>
-                <div key={dest.id} className='relative'>
-                    <p className='absolute top-0 left-1/2 z-10 transform -translate-x-1/2 text-center font-bold'>
-                        {dest.name}
-                    </p>
-                    <Image className='opacity-75 rounded-xl' alt={dest.name} src={`/${dest.name}.png`} width={200} height={120} />
-                </div>
-            )}
+        <div>
+            <div className='text-lg flex justify-center my-6'>Where are you travelling to?
+            </div>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex items-center rounded-full bg-figmaDark my-4 py-2">
+                    <div className="bg-figmaLightGreen rounded-full p-2 m-2 flex items-center">
+                        <MapPin color="white" />
+                    </div>
+                    <FormField
+                        control={form.control}
+                        name="dest"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input type="search" placeholder="Search..." className='bg-figmaDark text-lg' {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className="bg-figmaLightDark rounded-full p-2 m-2 flex items-center">
+                        <Search color="white" />
+                    </Button>
+                </form>
+            </Form>
+            <div className='text-sm text-gray-400 mt-14 mb-4'>Popular destinations in {location}
+            </div>
+            <div className='inline-grid grid-cols-2 gap-4'>
+                {filtered.length > 0 ? (
+                    filtered.map((dest) =>
+                        <div key={dest.id} className='relative'>
+                            <p className='absolute top-0 left-1/2 z-10 transform -translate-x-1/2 text-center font-bold'>
+                                {dest.name}
+                            </p>
+                            <Image className='opacity-75 rounded-xl' alt={dest.name} src={`/${dest.name}.png`} width={200} height={120} />
+                        </div>
+                    )
+                ) : (
+                    <p className='col-span-2 text-center text-sm text-gray-400'>No destinations fit your search :(</p>
+                )}
+            </div>
         </div>
     )
 }
