90a572113de079ab4af6c1af688108166a6c4869
Sdfeagt <rafalciechanski15@gmail.com>
2024-02-13 15:43:28 +0200

    Typo...

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Tripl planner',
+  title: 'Trip planner',
   description: 'Trip planning app',
 }
 
